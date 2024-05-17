cb12c4b3df64cba355040600c18a86c103125cde
nikol <nikol-di@yandex.ru>
2024-05-17 13:42:54 +0300

    about-us section

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import HeaderBlock from '@/components/header-block.vue'
 import OurServices from '@/components/our-services.vue'
 import CalculateBlock from '@/components/calculate-block.vue'
 import OurCars from '@/components/our-cars.vue'
+import AboutUs from '@/components/about-us.vue'
 
 </script>
 
@@ -13,6 +14,7 @@ import OurCars from '@/components/our-cars.vue'
   <our-services></our-services>
   <calculate-block></calculate-block>
   <our-cars></our-cars>
+  <about-us></about-us>
 </template>
 
 <style scoped>
